702a71c2c2f2341318f97f3c2367ea2deddce6a8
rohit2000lmp <rohit2000lmp@gmail.com>
2022-08-04 08:32:25 +0000

    Auto saving changes from StackRoute workbench on Thu Aug 04 2022 08:32:15 GMT+0000 (Coordinated Universal Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -7,7 +7,15 @@ package com.jap;
 public class SortingCities
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {	SortingCities object = new SortingCities();
+		String [] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+		int [] distance = {138,52,118,136,85,276,103,87,214,101};
+		String sortednames = object.convertToUpperCase(cityNames);
+        System.out.println( sortednames );
     }
+	public String convertToUpperCase(String[] cityNames )
+	{
+		String names = cityNames.toUpperCase();
+		return names;
+	}
 }
